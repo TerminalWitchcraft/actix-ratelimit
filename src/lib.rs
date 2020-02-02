d1e86e8786c71c2bf29c2080dae2b0b33a4709ce
Hitesh Paul <git@hiteshpaul.com>
2020-02-01 20:01:02 -0500

    Change store to an actor model

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,11 @@
 
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::rc::Rc;
-use std::sync::Arc;
+use std::marker::Send;
 use std::cell::RefCell;
 use std::future::Future;
 use std::task::{Context, Poll};
+use std::ops::Fn;
 use std::pin::Pin;
 
 use log::*;
@@ -20,89 +21,125 @@ use actix_web::{
     http::{HeaderName, HeaderValue},
 };
 
-mod timers;
+// mod timers;
 mod stores;
 
 /// Trait that implements functions required for buidling a RateLimiter.
 #[async_trait]
 pub trait RateLimit{
+    type Identifier: Fn(&ServiceRequest) -> String + 'static; 
+    type ErrorCallback: Fn(&mut HttpResponseBuilder) -> HttpResponseBuilder + 'static;
 
-    /// Get the identifier use to identify a request. Identifiers are used to identify a client. You can use
-    /// the `ServiceRequest` parameter to
-    /// extract one, or you could use your own identifier from different source.  Most commonly
-    /// used identifiers are IP address, cookies, cached data, etc
-    fn client_identifier(&self, req: &ServiceRequest) -> Result<String, AWError>;
+    // /// Get the identifier use to identify a request. Identifiers are used to identify a client. You can use
+    // /// the `ServiceRequest` parameter to
+    // /// extract one, or you could use your own identifier from different source.  Most commonly
+    // /// used identifiers are IP address, cookies, cached data, etc
+    // fn client_identifier(&self, req: &ServiceRequest) -> Result<String, AWError>;
 
-    /// Get the remaining number of accesses based on `key`. Here, `key` is used as the identifier
-    /// returned by `get_identifier` function. This functions queries the `store` to get the
-    /// reamining number of accesses.
-    async fn get(&self, key: &str) -> Result<Option<usize>, AWError>;
+    // /// Get the remaining number of accesses based on `key`. Here, `key` is used as the identifier
+    // /// returned by `get_identifier` function. This functions queries the `store` to get the
+    // /// reamining number of accesses.
+    // async fn get(&self, key: &str) -> Result<Option<usize>, AWError>;
+    //
+    // /// Sets the access count for the client identified by key to a value `value`. Again, key is
+    // /// the identifier returned by `get_identifier` function.
+    // async fn set(&self, key: String, value: usize, expiry: Option<Duration>) -> Result<(), AWError>;
+    //
+    // /// Get the expiry for the given key
+    // async fn expire(&self, key: &str) -> Result<Duration, AWError>;
+    //
+    // async fn remove(&self, key: &str) -> Result<usize, AWError>;
 
-    /// Sets the access count for the client identified by key to a value `value`. Again, key is
-    /// the identifier returned by `get_identifier` function.
-    async fn set(&self, key: String, value: usize, expiry: Option<Duration>) -> Result<(), AWError>;
-
-    /// Get the expiry for the given key
-    async fn expire(&self, key: &str) -> Result<Duration, AWError>;
-
-    async fn remove(&self, key: &str) -> Result<usize, AWError>;
-
-    /// Callback to execute after each processing of the middleware. You can add your custom
-    /// implementation according to your needs. For example, if you want to log client which used
-    /// 95% of the quota, you could do so by:
-    #[allow(unused_mut)]
-    fn error_callback(&self, mut response: HttpResponseBuilder) -> HttpResponseBuilder {
-        response
-    }
+    // /// Callback to execute after each processing of the middleware. You can add your custom
+    // /// implementation according to your needs. For example, if you want to log client which used
+    // /// 95% of the quota, you could do so by:
+    // #[allow(unused_mut)]
+    // fn error_callback(&self, mut response: HttpResponseBuilder) -> HttpResponseBuilder {
+    //     response
+    // }
 
 }
 
+
+pub enum Messages{
+    Get(String),
+    Set {key: String, value: usize, expiry: Option<Duration>},
+    Expire(String),
+    Remove(String)
+}
+
+impl Message for Messages{
+    type Result = Responses;
+}
+
+pub enum Responses{
+    Get(Pin<Box<dyn Future<Output=Option<usize>> + Send>>),
+    Set(Pin<Box<dyn Future<Output=()> + Send>>),
+    Expire(Pin<Box<dyn Future<Output=Duration> + Send>>),
+    Remove(Pin<Box<dyn Future<Output=usize> + Send>>),
+}
+
+impl<A, M> MessageResponse<A, M> for Responses
+where
+    A: Actor,
+    M: Message<Result = Responses>,
+{
+    fn handle<R: ResponseChannel<M>>(self, _: &mut A::Context, tx: Option<R>) {
+        if let Some(tx) = tx {
+            tx.send(self);
+        }
+    }
+}
 
 /// Type that implements the ratelimit middleware. This accepts `interval` which specifies the
 /// window size, `max_requests` which specifies the maximum number of requests in that window, and
 /// `store` which is essentially a data store used to store client access information. Store is any
 /// type that implements `RateLimit` trait.
-pub struct RateLimiter<T, A>
+pub struct RateLimiter<T>
 where
-    T: RateLimit + 'static,
-    A: Handler<timers::Task<String>>,
-    A::Context: ToEnvelope<A, timers::Task<String>>
+    T: Handler<Messages> + 'static,
+    T::Context: ToEnvelope<T, Messages>
 {
     interval: Duration,
     max_requests: usize,
-    store: Arc<T>,
-    timer: Option<Addr<A>>
+    store: Rc<Addr<T>>,
+    identifier: Rc<Box<dyn Fn(&ServiceRequest) -> String>>
 }
 
-impl Default for RateLimiter<stores::MemoryStore, timers::TimerActor<stores::MemoryStore>>
+impl Default for RateLimiter<stores::MemoryStore>
 {
     fn default() -> Self {
-        let store = Arc::new(stores::MemoryStore::new());
-        let store_clone = store.clone();
+        let store = stores::MemoryStore::new();
+        let identifier = |req: &ServiceRequest| {
+            let soc_addr = req.peer_addr().unwrap();
+            soc_addr.ip().to_string()
+        };
         RateLimiter{
             interval: Duration::from_secs(0),
             max_requests: 0,
-            store: store,
-            timer: Some(timers::TimerActor::start(Duration::from_secs(0), store_clone))
+            store: Rc::new(store.start()),
+            identifier: Rc::new(Box::new(identifier))
         }
     }
 }
 
-impl<T, A> RateLimiter<T, A>
+impl<T> RateLimiter<T>
 where
-    T: RateLimit + 'static,
-    A: Handler<timers::Task<String>>,
-    A::Context: ToEnvelope<A, timers::Task<String>>
+    T: Handler<Messages> + 'static,
+    <T as Actor>::Context: ToEnvelope<T, Messages>
 {
 
     /// Creates a new instance of `RateLimiter`.
-    pub fn new(store: T) -> Self {
-        let store = Arc::new(store);
+    pub fn new(store: Addr<T>) -> Self {
+        let identifier = |req: &ServiceRequest| {
+            let soc_addr = req.peer_addr().unwrap();
+            soc_addr.ip().to_string()
+        };
         RateLimiter{
             interval: Duration::from_secs(0),
             max_requests: 0,
-            store: store,
-            timer: None
+            store: Rc::new(store),
+            identifier: Rc::new(Box::new(identifier))
         }
     }
 
@@ -118,19 +155,12 @@ where
         self
     }
 
-    /// Specify actix actor to handle delayes task
-    pub fn with_timer(mut self, addr: Addr<A>) -> Self {
-        self.timer = Some(addr);
-        self
-    }
-
 }
 
-impl<T: 'static, A, S, B> Transform<S> for RateLimiter<T, A>
+impl<T, S, B> Transform<S> for RateLimiter<T>
 where
-    T: RateLimit,
-    A: Handler<timers::Task<String>>,
-    A::Context: ToEnvelope<A, timers::Task<String>>,
+    T: Handler<Messages> + 'static,
+    T::Context: ToEnvelope<T, Messages>,
     S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = AWError>  + 'static,
     S::Future: 'static,
     B: 'static ,
@@ -139,49 +169,43 @@ where
     type Response = ServiceResponse<B>;
     type Error = S::Error; 
     type InitError = ();
-    type Transform = RateLimitMiddleware<S, T, A>;
+    type Transform = RateLimitMiddleware<S, T>;
     type Future = Ready<Result<Self::Transform, Self::InitError>>;
 
     fn new_transform(&self, service: S) -> Self::Future
     {
-        let timer = match &self.timer{
-            Some(c) => {Some(c.clone())}, 
-            None => None
-        };
         ok(RateLimitMiddleware {
             service: Rc::new(RefCell::new(service)),
             store: self.store.clone(),
             max_requests: self.max_requests,
             interval: self.interval.as_secs(),
-            timer: timer
+            get_identifier: self.identifier.clone(),
         })
     }
 }
 
 
 /// Middleware for RateLimiter.
-pub struct RateLimitMiddleware<S, T, A>
+pub struct RateLimitMiddleware<S, T>
 where
     S: 'static,
-    T: RateLimit + 'static,
-    A: Actor + Handler<timers::Task<String>>,
+    T: Handler<Messages> + 'static,
 {
     service: Rc<RefCell<S>>,
-    store: Arc<T>,
+    store: Rc<Addr<T>>,
     // Exists here for the sole purpose of knowing the max_requests and interval from RateLimiter
     max_requests: usize,
     interval: u64,
-    timer: Option<Addr<A>>
+    get_identifier: Rc<Box<dyn Fn(&ServiceRequest) -> String + 'static>>,
 }
 
-impl <T, S, B, A> Service for RateLimitMiddleware<S, T, A>
+impl <T, S, B> Service for RateLimitMiddleware<S, T>
 where
-    T: RateLimit + 'static,
+    T: Handler<Messages> + 'static,
     S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = AWError> + 'static,
     S::Future: 'static,
     B: 'static,
-    A: Handler<timers::Task<String>>,
-    A::Context: ToEnvelope<A, timers::Task<String>>,
+    T::Context: ToEnvelope<T, Messages>,
 {
     type Request = ServiceRequest;
     type Response = ServiceResponse<B>;
@@ -199,29 +223,34 @@ where
         let mut srv = self.service.clone();
         let max_requests = self.max_requests;
         let interval = Duration::from_secs(self.interval);
-        let timer = match &self.timer{
-            Some(c) => Some(c.clone()),
-            None => None
-        };
+        let get_identifier = self.get_identifier.clone();
         Box::pin(async move {
-            let identifier: String = store.client_identifier(&req)?;
-            let remaining: Option<usize> = store.get(&identifier).await?;
+            let identifier: String = (get_identifier)(&req);
+            let remaining: Responses = store.send(Messages::Get(String::from(&identifier))).await?;
             match remaining{
                 // Existing entry in store
-                Some(c) => {
-                    let reset = store.expire(&identifier).await?;
+                Responses::Get(c) => {
+                    let c:usize = c.await.unwrap();
+                    let expiry = store.send(Messages::Expire(String::from(&identifier))).await?;
+                    let reset: Duration = match expiry {
+                        Responses::Expire(dur) => dur.await,
+                        _ => {
+                            let now = SystemTime::now();
+                            now.duration_since(UNIX_EPOCH).unwrap() + interval
+                        }
+                    };
                     if c == 0 {
                         info!("Limit exceeded for client: {}", &identifier);
-                        let response = HttpResponse::TooManyRequests();
-                        let mut response = store.error_callback(response);
+                        let mut response = HttpResponse::TooManyRequests();
+                        // let mut response = (error_callback)(&mut response);
                         response.set_header("x-ratelimit-limit", max_requests.to_string());
                         response.set_header("x-ratelimit-remaining", c.to_string());
                         response.set_header("x-ratelimit-reset", reset.as_secs().to_string());
-                        Err(store.error_callback(response).into())
+                        Err(response.into())
                     } else {
                         // Execute the req
                         // Decrement value
-                        store.set(identifier, c + 1, None).await?;
+                        store.send(Messages::Set{key: identifier, value: c + 1, expiry: None}).await?;
                         let fut = srv.call(req);
                         let mut res = fut.await?;
                         let headers = res.headers_mut();
@@ -242,15 +271,14 @@ where
                     }
                 },
                 // New client, create entry in store
-                None => {
+                _ => {
                     let now = SystemTime::now();
-                    store.set(String::from(&identifier), max_requests,
-                        Some(now.duration_since(UNIX_EPOCH).unwrap() + interval)).await?;
+                    store.send(Messages::Set{
+                        key: String::from(&identifier),
+                        value: max_requests,
+                        expiry: Some(now.duration_since(UNIX_EPOCH).unwrap() + interval)
+                    }).await?;
                     // [TODO]Send a task to delete key after `interval` if Actor is preset
-                    if let Some(c) = timer{
-                        let task = timers::Task{key: String::from(identifier)};
-                        c.do_send(task);
-                    }
                     let fut = srv.call(req);
                     let mut res = fut.await?;
                     let headers = res.headers_mut();
--- src/stores/memory.rs
+++ src/stores/memory.rs
@@ -1,11 +1,14 @@
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use actix::prelude::*;
 use dashmap::DashMap;
+use futures::future::{self, Future};
+use std::pin::Pin;
 use async_trait::async_trait;
 use actix_web::dev::ServiceRequest;
 use actix_web::Error as AWError;
 use actix_web::web::HttpResponse;
 
-use crate::RateLimit;
+use crate::{Messages, Responses};
 
 pub struct MemoryStore
 {
@@ -25,57 +28,108 @@ impl MemoryStore
             inner: DashMap::with_capacity(capacity)
         }
     }
+
 }
 
-#[async_trait]
-impl RateLimit for MemoryStore
-{
-    fn client_identifier(&self, req: &ServiceRequest) -> Result<String, AWError> {
-        let soc_addr = req.peer_addr().ok_or(AWError::from(()))?;
-        Ok(soc_addr.ip().to_string())
-    }
+impl Actor for MemoryStore{
+    type Context = Context<Self>;
+}
 
-    async fn get(&self, key: &str) -> Result<Option<usize>, AWError> {
-        if self.inner.contains_key(key){
-            let val = self.inner.get(key).unwrap();
-            let val = val.value().0;
-            Ok(Some(val))
-        } else {
-            Ok(None)
-        }
-    }
-
-    async fn set(&self, key: String, val: usize, expiry: Option<Duration>) -> Result<(), AWError> {
-        if let Some(c) = expiry{
-            // New entry, sets the key
-            self.inner.insert(key, (val, c)).unwrap();
-        } else {
-            // Only update the request count
-            let data = self.inner.get(&key).unwrap();
-            let data = data.value().1;
-            let new_data = (val, Duration::from(data));
-            self.inner.insert(key, new_data).unwrap();
-        }
-        Ok(())
-    }
-
-    async fn expire(&self, key: &str) -> Result<Duration, AWError> {
-        match self.inner.get(key){
-            Some(c) => {
+impl Handler<Messages> for MemoryStore {
+    type Result = Responses;
+    fn handle(&mut self, msg: Messages, ctx: &mut Self::Context) -> Self::Result{
+        match msg {
+            Messages::Set{key, value, expiry} => {
+                if let Some(dur) = expiry{
+                    let future_key = String::from(&key);
+                    self.inner.insert(key, (value, dur)).unwrap();
+                    ctx.run_later(dur, move |a, _|{
+                        a.inner.remove(&future_key);
+                    });
+                } else {
+                    let data = self.inner.get(&key).unwrap();
+                    let expire = data.value().1;
+                    let new_data = (value, expire);
+                    self.inner.insert(key, new_data).unwrap();
+                }
+                let fut = future::ready(());
+                Responses::Set(Box::pin(fut))
+            },
+            Messages::Get(key) => {
+                if self.inner.contains_key(&key) {
+                    let val = self.inner.get(&key).unwrap();
+                    let val = val.value().0;
+                    Responses::Get(Box::pin(future::ready(Some(val))))
+                    // Responses::Get(Pin::new(Box::new(future::ready(Some(val)))))
+                } else {
+                    Responses::Get(Box::pin(future::ready(None)))
+                }
+            },
+            Messages::Expire(key) => {
+                let c = self.inner.get(&key).unwrap();
                 let dur = c.value().1;
                 let now = SystemTime::now();
                 let dur = dur - now.duration_since(UNIX_EPOCH).unwrap();
-                Ok(dur)
+                Responses::Expire(Box::pin(future::ready(dur)))
             },
-            None => {
-                Err(HttpResponse::InternalServerError().into())
+            Messages::Remove(key) => {
+                let val = self.inner.remove::<String>(&key).unwrap();
+                let val = val.1;
+                Responses::Remove(Box::pin(future::ready(val.0)))
             }
         }
     }
-
-    async fn remove(&self, key: &str) -> Result<usize, AWError> {
-        let val = self.inner.remove::<String>(&key.to_string()).unwrap();
-        let val = val.1;
-        Ok(val.0)
-    }
 }
+
+// #[async_trait]
+// impl RateLimit for MemoryStore
+// {
+    // fn client_identifier(&self, req: &ServiceRequest) -> Result<String, AWError> {
+    //     let soc_addr = req.peer_addr().ok_or(AWError::from(()))?;
+    //     Ok(soc_addr.ip().to_string())
+    // }
+    //
+    // async fn get(&self, key: &str) -> Result<Option<usize>, AWError> {
+    //     if self.inner.contains_key(key){
+    //         let val = self.inner.get(key).unwrap();
+    //         let val = val.value().0;
+    //         Ok(Some(val))
+    //     } else {
+    //         Ok(None)
+    //     }
+    // }
+    //
+    // async fn set(&self, key: String, val: usize, expiry: Option<Duration>) -> Result<(), AWError> {
+    //     if let Some(c) = expiry{
+    //         // New entry, sets the key
+    //         self.inner.insert(key, (val, c)).unwrap();
+    //     } else {
+    //         // Only update the request count
+    //         let data = self.inner.get(&key).unwrap();
+    //         let data = data.value().1;
+    //         let new_data = (val, Duration::from(data));
+    //         self.inner.insert(key, new_data).unwrap();
+    //     }
+    //     Ok(())
+    // }
+    //
+    // async fn expire(&self, key: &str) -> Result<Duration, AWError> {
+    //     match self.inner.get(key){
+    //         Some(c) => {
+    //             let dur = c.value().1;
+    //             let now = SystemTime::now();
+    //             let dur = dur - now.duration_since(UNIX_EPOCH).unwrap();
+    //             Ok(dur)
+    //         },
+    //         None => {
+    //             Err(HttpResponse::InternalServerError().into())
+    //         }
+    //     }
+    // }
+    //
+    // async fn remove(&self, key: &str) -> Result<usize, AWError> {
+    //     let val = self.inner.remove::<String>(&key.to_string()).unwrap();
+    //     let val = val.1;
+    //     Ok(val.0)
+    // }
+// }
