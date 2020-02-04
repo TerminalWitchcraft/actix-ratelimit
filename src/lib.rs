6477425bb417194175bb05f198b9cc611db818be
Hitesh Paul <git@hiteshpaul.com>
2020-02-04 15:27:10 -0500

    Add basic documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,79 @@
-//! Rate limiting middleware framework for actix-web
+//! Rate limiting middleware framework for [actix-web](https://actix.rs/)
+//!
+//! This crate provides an asynchronous rate-limiter middleware based on [Actor](https://www.wikiwand.com/en/Actor_model) 
+//! model which can be wraped around an Actix application. Middleware contains a `store` which is used to 
+//! save an identifier derived from client request.
+//!
+//! # Usage
+//! Add this to your `Cargo.toml`:
+//! ```
+//! actix-web = "2"
+//! actix-rt = "1"
+//! actix-ratelimit = "0.2.0"
+//! ```
+//!
+//! Minimal example:
+//!
+//! ```
+//! use std::time::Duration;
+//! use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+//! use actix_ratelimit::middleware::RateLimiter;
+//! use actix_ratelimit::stores::MemoryStore;
+//!
+//! async fn greet(req: HttpRequest) -> impl Responder{
+//!     let name = req.match_info().get("name").unwrap_or("World!");
+//!     format!("Hello {}!", &name)
+//! }
+//!
+//! #[actix_rt::main]
+//! async fn main() -> std::io::Result<()> {
+//!     HttpServer::new(||{
+//!         App::new()
+//!             // Register the rate-limiter middleware
+//!             // which allows for maximum of 
+//!             // 100 requests per minute per client
+//!             .wrap(
+//!                 RateLimiter::new(MemoryStore::default().start())
+//!                 .with_interval(Duration::from_secs(60))
+//!                 .with_max_requests(100)
+//!             )
+//!             .route("/", web::get().to(greet))
+//!             .route("/{name}", web::get().to(greet))
+//!     })
+//!     .bind("127.0.0.1:8000")?
+//!     .run()
+//!     .await
+//! }
+//! ```
+//! Sending a request returns a response with the ratelimiting headers: 
+//! ```
+//! echo("Hello");
+//! ```
+//!
+//! # Backends
+//!
+//!
+//! ## Supported
+//! - In-memory (based on [dashmap](https://github.com/xacrimon/dashmap))
+//! - Redis (based on [redis-rs](https://github.com/mitsuhiko/redis-rs))
+//!
+//! ## Planned
+//! - Memcached (not yet implemented)
+//!
+//! # Implementing your own store
+//! 
+//! Lorem ipsum
+//!
+//! # Status
+//! This project has not reached `v1.0`, so api instability and breaking changes are to be expected
+//! till then.
+//!
+//! # LICENSE
+//! This project is licensed under MIT license.
 
 pub mod errors;
-pub mod stores;
 pub mod middleware;
+pub mod stores;
 use errors::ARError;
 
 #[cfg(feature = "default")]
@@ -16,7 +87,6 @@ use std::pin::Pin;
 use std::time::Duration;
 
 use actix::dev::*;
-
 
 pub enum Messages {
     Get(String),
@@ -57,4 +127,3 @@ where
         }
     }
 }
-
