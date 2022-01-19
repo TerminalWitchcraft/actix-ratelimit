9fee9a82860c6121973092fc4fd52ae2c7352b33
twin <twin@twin.com>
2022-01-19 02:06:05 -0700

    thiserror refactor

--- src/errors.rs
+++ src/errors.rs
@@ -1,39 +1,39 @@
 //! Errors that can occur during middleware processing stage
 use actix_web::error::Error as AWError;
 use actix_web::web::HttpResponse;
-use failure::{self, Fail};
+use thiserror::Error;
 use log::*;
 
 /// Custom error type. Useful for logging and debugging different kinds of errors.
 /// This type can be converted to Actix Error, which defaults to
 /// InternalServerError
 ///
-#[derive(Debug, Fail)]
+#[derive(Error,Debug)]
 pub enum ARError {
     /// Store is not connected
-    #[fail(display = "store not connected")]
+    #[error("store not connected")]
     NotConnected,
 
     /// Store is disconnected after initial successful connection
-    #[fail(display = "store disconnected")]
+    #[error("store disconnected")]
     Disconnected,
 
     /// Read/Write error on store
-    #[fail(display = "read/write operatiion failed: {}", _0)]
+    #[error("read/write operatiion failed: {}", _0)]
     ReadWriteError(String),
 
     /// Could be any kind of IO error
-    #[fail(display = "unknown error: {}", _0)]
-    UnknownError(std::io::Error),
+    #[error("unknown error: {}", _0)]
+    UnknownError(#[from] std::io::Error),
 
     /// Identifier error
-    #[fail(display = "client identification failed")]
+    #[error("client identification failed")]
     IdentificationError,
 }
 
 impl From<ARError> for AWError {
-    fn from(err: ARError) -> AWError {
-        error!("{}", &err);
-        HttpResponse::InternalServerError().into()
+    fn from(err: ARError) -> Self {
+            error!("{}", &err);
+            HttpResponse::InternalServerError().into()
     }
 }
